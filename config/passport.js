7427f1f86b1b9aa1fa03b759fcf2da9d6299b4a2
programmer-santosh-main <santosh.thpa.a1@gmail.com>
2025-12-26 14:23:01 +0545

    afsaf

--- config/passport.js
+++ config/passport.js
@@ -85,7 +85,7 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: "/api/auth/facebook/callback",
+      callbackURL: `${process.env.BACKEND_URL}/api/api/auth/facebook`,
       profileFields: ["id", "displayName", "photos", "email"],
     },
     async (accessToken, refreshToken, profile, done) => {
